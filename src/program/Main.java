f4b4119ab9d1781b3ea3138533c6cdd3bebf239e
inbaea <49175916+inbaea@users.noreply.github.com>
2022-09-02 17:25:17 +0900

    new project

---
+++ src/program/Main.java
@@ -0,0 +1,14 @@
+package program;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello, World!");
+		System.out.print("이름입력 : ");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영합니다~");
+	}
+
+}
